26fe128bd3fa53ee5cb6a22fe6eec14fd84efe97
Kanan Farzali <kanan.farzali@gmail.com>
2024-06-26 12:33:39 +0200

    layout creation

--- app/page.js
+++ app/page.js
@@ -1,5 +1,13 @@
+import Link from 'next/link';
+
 const HomePage = () => {
-  return <h1>Hello Next!</h1>;
+  return (
+    <div>
+      <h1>The Wild Oasis. Welcome to the paradise</h1>
+
+      <Link href="/cabins">Explore luxury cabins</Link>
+    </div>
+  );
 };
 
 export default HomePage;
