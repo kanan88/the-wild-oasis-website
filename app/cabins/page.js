30690cd99e9966adc1a32c1db466f15052427d24
Kanan Farzali <kanan.farzali@gmail.com>
2024-06-26 23:57:19 +0200

    fetching data with server component

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,7 +1,18 @@
-import React from 'react';
+const CabinsPage = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
 
-const CabinsPage = () => {
-  return <h1>Cabins Page</h1>;
+  return (
+    <div>
+      <h1>Cabins Page</h1>
+
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default CabinsPage;
