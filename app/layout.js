159af700571aa92d9f11d439658751f2952e1fa2
Kanan Farzali <kanan.farzali@gmail.com>
2024-06-27 23:30:48 +0200

    improving the navigation and root layout

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,7 @@
-import Logo from './_components/Logo';
-import Navigation from './_components/Navigation';
-
 import { Josefin_Sans } from 'next/font/google';
 
 import '@/app/_styles/globals.css';
+import Header from './_components/Header';
 
 const josefin = Josefin_Sans({
   subsets: ['latin'],
@@ -23,15 +21,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
       >
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
+        <Header />
 
-        <main>{children}</main>
-        <footer>&copy; 2024 The Wild Oasis </footer>
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   );
