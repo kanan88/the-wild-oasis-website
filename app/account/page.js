7d8aef3492648cabfe5d4007b94b669b164ca337
Kanan Farzali <kanan.farzali@gmail.com>
2024-06-28 00:59:43 +0200

    adding nested routes and pages

--- app/account/page.js
+++ app/account/page.js
@@ -3,7 +3,11 @@ export const metadata = {
 };
 
 const AccountPage = () => {
-  return <div>AccountPage</div>;
+  return (
+    <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+      Welcome, Ken!
+    </h2>
+  );
 };
 
 export default AccountPage;
