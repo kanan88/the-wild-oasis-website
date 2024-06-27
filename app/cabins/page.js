0ca4ab97e23c4ea4c36140e2052201ae89f4240a
Kanan Farzali <kanan.farzali@gmail.com>
2024-06-27 21:45:38 +0200

    project organization

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,20 +1,9 @@
-import Counter from '../components/Counter';
+import Counter from '@/app/_components/Counter';
 
-const CabinsPage = async () => {
-  const res = await fetch('https://jsonplaceholder.typicode.com/users');
-  const data = await res.json();
-
+const CabinsPage = () => {
   return (
     <div>
       <h1>Cabins Page</h1>
-
-      <ul>
-        {data.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
-
-      <Counter users={data} />
     </div>
   );
 };
