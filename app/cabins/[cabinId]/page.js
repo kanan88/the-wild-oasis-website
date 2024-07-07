a3fe49c406b37310b8ebcbbf4aaa765a414914be
Kanan Farzali <kanan.farzali@gmail.com>
2024-07-07 22:06:20 +0200

    making dynamic pages static with generate StaticParams

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -1,4 +1,4 @@
-import { getCabin } from '@/app/_lib/data-service';
+import { getCabin, getCabins } from '@/app/_lib/data-service';
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from '@heroicons/react/24/solid';
 import Image from 'next/image';
 
@@ -8,6 +8,16 @@ export const generateMetadata = async ({ params }) => {
   return {
     title: `Cabin ${name}`,
   };
+};
+
+export const generateStaticParams = async () => {
+  const cabins = await getCabins();
+
+  const ids = cabins.map((cabin) => ({
+    cabinId: String(cabin.id),
+  }));
+
+  return ids;
 };
 
 const Page = async ({ params }) => {
